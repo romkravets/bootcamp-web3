5a80822e5e7c85d27b8ad42f7335e25787b5e4d3
Roman Kravets <romann.kravets@gmail.com>
2025-04-06 16:30:17 +0300

    add

--- send-sol.ts
+++ send-sol.ts
@@ -13,7 +13,7 @@ import {
   const secretKey = process.env.SECRET_KEY ? JSON.parse(process.env.SECRET_KEY) : null;
 
 if (!secretKey || !Array.isArray(secretKey) || secretKey.length !== 64) {
-    console.error("❌ Secret key має неправильний формат або розмір:", secretKey.length);
+    console.error("Error", secretKey.length);
     process.exit(1);
 }
 
@@ -21,7 +21,6 @@ const secretKeyUint8Array = new Uint8Array(secretKey);
 const keypair = Keypair.fromSecretKey(secretKeyUint8Array);
 
 const connection = new Connection(clusterApiUrl("devnet"));
-//HTDpDKuxL31cKmhpN8Y2hQF8K1gAcsyKATos75YCYzi
 
 const rec =  new PublicKey('HTDpDKuxL31cKmhpN8Y2hQF8K1gAcsyKATos75YCYzi')
 
